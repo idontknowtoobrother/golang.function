7aa26f5983ce857952781469894b5c773d69f9ee
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 18:02:21 +0700

    function recieve params then process and return out

--- index.go
+++ index.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	displayPlayerData("John", 100)
+
+	const a, b = 5, 10
+	fmt.Println("a + b = ", plus(a, b))
 }
 
 func displayPlayerData(playerName string, playerMoney int) {
 	fmt.Println("Player Name: ", playerName, "Player Money: ", playerMoney)
+}
+
+func plus(n1 int, n2 int) int {
+	return n1 + n2
 }
