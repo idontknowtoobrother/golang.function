a3cc95f11b04c9a4b9e9c0551f6832a5ec8775f7
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 18:08:40 +0700

    function return more than 1 variables

--- index.go
+++ index.go
@@ -9,6 +9,12 @@ func main() {
 	fmt.Println("a + b = ", plus(a, b))
 
 	fmt.Printf("%d + %d = %d", a, b, plus(a, b))
+
+	var c, d = 10, 20
+	fmt.Printf("\nc = %d, d = %d", c, d)
+
+	c, d = swap(c, d)
+	fmt.Printf("\nafter swap\nc = %d, d = %d\n", c, d)
 }
 
 func displayPlayerData(playerName string, playerMoney int) {
@@ -17,4 +23,8 @@ func displayPlayerData(playerName string, playerMoney int) {
 
 func plus(n1 int, n2 int) int {
 	return n1 + n2
+}
+
+func swap(n1 int, n2 int) (int, int) {
+	return n2, n1
 }
